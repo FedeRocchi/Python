42428ce96648f796802e4b2ce6a7a27514d7ad57
FedeRocchi <110907542+FedeRocchi@users.noreply.github.com>
2022-08-09 10:30:26 -0300

    a

---
+++ EJ3.py
@@ -0,0 +1,20 @@
+print("--------------------------")
+print("Ejercicio 3")
+print("--------------------------")
+
+print("Ingrese el número de respuestas correctas:")
+RC = int(input())
+print("Ingrese el número de respuestas en blanco:")
+RB = int(input())
+print("Ingrese el número de respuestas incorrectas:")
+RI = int(input())
+
+PRC = int(RC*3)
+
+PRB = int(RB*0)
+
+PRI = int(RI*-1)
+
+PF = int(PRC+PRB+PRI)
+
+print("El puntaje final es:", PF)
