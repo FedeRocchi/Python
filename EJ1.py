d0572c205266a98a3af0c34ac159cba845967dc9
FedeRocchi <110907542+FedeRocchi@users.noreply.github.com>
2022-08-09 10:10:34 -0300

    ej1

--- EJ1.py
+++ EJ1.py
@@ -6,7 +6,7 @@ print("-------------------------------------------------")
 print("Ingrese la velocidad y el tiempo de la unnidad móvil")
 #convirtiendo entrada a Entero
 V = float( input("Velocidad: "))
-T = int( intput("Tiempo: "))
+T = int( input("Tiempo: "))
 
 #Proceso
 D = V*T
---
+++ EJ2.py
@@ -0,0 +1,17 @@
+print("------------------------")
+print("Ejercicio 2: Calcular promedio")
+print("------------------------")
+#Entradas
+print("Ingrese las 3 del alumno")
+n1 = int(input("N1: "))
+n2 = int(input("N2: "))
+n3 = int(input("N3: "))
+
+#Proceso
+
+P = float((n1+n2+n3)/3)
+
+#Salida
+
+print("Salida: ")
+print(P)
