03a1263a513a2bb2c9501a3a1a6057f62cb4b7ee
FedeRocchi <110907542+FedeRocchi@users.noreply.github.com>
2022-08-09 09:12:08 -0300

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,15 @@
+print("-------------------------------------------------")
+print("Ejercicio1:: CALCULAR LA DISTANCIA.")
+print("-------------------------------------------------")
+
+#Entradas
+print("Ingrese la velocidad y el tiempo de la unnidad móvil")
+#convirtiendo entrada a Entero
+V = float( input("Velocidad: "))
+T = int( intput("Tiempo: "))
+
+#Proceso
+D = V*T
+
+#Salida
+print(D)
